28d35471ad2db8064f70ac51a159a2c9cb83f159
C. Scott Ananian <cscott@cscott.net>
2013-11-12 16:49:58 -0500

    Flesh out skeleton of LaTeX->PDF converter.

--- bin/parsoid-latex.js
+++ bin/parsoid-latex.js
@@ -23,5 +23,50 @@ if (program.args.length > 1) {
 	console.error('Too many arguments.');
 	return 1;
 }
+
+var domino = require('domino');
+var fs = require('fs');
+var gammalatex = require('gammalatex');
+var request = require('request');
+var url = require('url');
+var util = require('util');
+
 var title = program.args[0];
-console.log(title);
+// Fetch parsoid source for this page.
+var apiURL = url.resolve(program.api, program.prefix + '/' + title);
+
+function log() {
+	// en/disable log messages here
+	//console.error.apply(console, arguments);
+}
+
+
+log('Fetching from Parsoid');
+request(apiURL, function(error, response, body) {
+	if (error || response.statusCode !== 200) {
+		console.error("Error fetching Parsoid source:", apiURL);
+		return 1;
+	}
+	// parse to DOM
+	log('Converting to DOM');
+	var dom = domino.createDocument(body);
+	// ok, generate LaTeX!
+	log('Converting to LaTeX');
+	var latexOutput = parsoidlatex.convert(dom);
+	// compile to PDF!
+	log('Compiling to PDF with xelatex');
+	gammalatex.setCompileCommand({
+		command: "xelatex",
+		options: ["-interaction=nonstopmode", "-halt-on-error"]
+	});
+	gammalatex.parse(latexOutput, function(err, readStream) {
+		log('Saving PDF');
+		if (err) throw err;
+		var writeStream = process.stdout;
+		if (program.output) {
+			writeStream = fs.createWriteStream(program.output);
+		}
+		readStream.pipe(writeStream);
+		return 0;
+	});
+});
---
+++ fonts/README.md
@@ -0,0 +1,8 @@
+See
+http://mirror.math.ku.edu/tex-archive/macros/latex/contrib/fontspec/fontspec.pdf
+for details on font selection with xelatex.
+
+The fonts in this directory are Gentium: http://scripts.sil.org/gentium
+
+This is a good Latin font, but we would still need high quality CJK (etc)
+fonts.
