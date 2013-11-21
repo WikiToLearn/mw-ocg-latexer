b6233c6bf15ef2520c1dbe3fef1042bbc5beac15
C. Scott Ananian <cscott@cscott.net>
2013-11-21 18:54:56 -0500

    Don't try to test XeLaTeX compilation if XeLaTeX is not installed.
    Change-Id: I06a1253703b1b094abe7d7b4b8842990d22a7ae2

--- test/samples.js
+++ test/samples.js
@@ -5,17 +5,36 @@ var path = require('path');
 
 var latexer = require('../');
 
+// determine if xelatex is installed
+var checkXeLaTeX = function() {
+	var has = false;
+	process.env.PATH.split(path.delimiter).forEach(function(p) {
+		/* jshint bitwise: false */
+		try {
+			var st = fs.statSync(path.join(p, 'xelatex'));
+			if (st.isFile() && (st.mode & 0111) !== 0) {
+				/* it's an executable file */
+				has = true;
+			}
+		} catch (e) { /* nope, xelatex not here */ }
+	});
+	return has;
+};
+
 // ensure that we don't crash on any of our sample inputs
 describe("Basic crash test", function() {
+	var hasXeLaTeX = checkXeLaTeX();
 	['tao.zip', 'hurricanes.zip', 'us.zip'].forEach(function(bundle) {
 		describe(bundle, function() {
-			it('should compile to PDF', function(done) {
+			var dest = hasXeLaTeX ? 'PDF' : 'tex';
+			it('should compile to '+dest, function(done) {
 				this.timeout(0);
 				var filename = path.join(__dirname, '..', 'samples', bundle);
 				return latexer.convert({
 					bundle: filename,
 					output: filename + '.pdf',
 					size: 'letter',
+					debug: !hasXeLaTeX,
 					log: function() { /* suppress logging */ }
 				}).then(function(statusCode) {
 					assert.equal(statusCode, 0);
